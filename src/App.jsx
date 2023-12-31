c8e968be5477bfe6c3c2d33938e04bc9966a5638
Évelyne Lachance <eslachance@gmail.com>
2023-12-30 22:32:17 -0500

    Initial code commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { Search } from "@/components";
+
+function App() {
+  return (
+    <>
+      <Search />
+      <div className="m-2 p-2 flex items-center gap-2 border-1 border-solid w-fit rounded">
+        <span className="i-ic-sharp-edit-note" />
+        Edit me!
+      </div>
+    </>
+  );
+}
+
+export default App;
